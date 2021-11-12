c0c33b1ae950dfcb4333cbf0b80eb3eb1d74cd2a
Tec-Dynamics <support@tecdynamics.co.uk>
2021-11-12 17:45:28 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Installer | A Web Installer [Package](https://packagist.org/packages/tecdynamics/installer) 
+ 
+- [License](#license)
+
+
+
+## License
+
+Laravel Web Installer is licensed under the MIT license. Enjoy!
---
+++ src/Controllers/UpdateController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace tecdynamics\installer\Controllers;
+
+use Illuminate\Routing\Controller;
+use tecdynamics\installer\Helpers\InstalledFileManager;
+use tecdynamics\installer\Helpers\DatabaseManager;
+
+class UpdateController extends Controller
+{
+    use \tecdynamics\installer\Helpers\MigrationsHelper;
+
+    /**
+     * Display the updater welcome page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function welcome()
+    {
+        return view('vendor.installer.update.welcome');
+    }
+
+    /**
+     * Display the updater overview page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function overview()
+    {
+        $migrations = $this->getMigrations();
+        $dbMigrations = $this->getExecutedMigrations();
+
+        return view('vendor.installer.update.overview', ['numberOfUpdatesPending' => count($migrations) - count($dbMigrations)]);
+    }
+
+    /**
+     * Migrate and seed the database.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function database()
+    {
+        $databaseManager = new DatabaseManager;
+        $response = $databaseManager->migrateAndSeed();
+
+        return redirect()->route('LaravelUpdater::final')
+                         ->with(['message' => $response]);
+    }
+
+    /**
+     * Update installed file and display finished view.
+     *
+     * @param InstalledFileManager $fileManager
+     * @return \Illuminate\View\View
+     */
+    public function finish(InstalledFileManager $fileManager)
+    {
+        $fileManager->update();
+
+        return view('vendor.installer.update.finished');
+    }
+}
